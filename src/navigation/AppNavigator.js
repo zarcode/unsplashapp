b2d57515882a227c02cbeb73ca9e5832997d4bf5
Zarko Dencic <denciczarko@gmail.com>
2018-03-15 11:20:45 +0100

    navigation setup

---
+++ src/navigation/AppNavigator.js
@@ -0,0 +1,19 @@
+import {
+    addNavigationHelpers,
+    TabBarBottom,
+    StackNavigator,
+    TabNavigator
+} from "react-navigation";
+import PhotosListScreen from "../containers/PhotosListScreen";
+import PhotoSingleScreen from "../containers/PhotoSingleScreen";
+
+export const AppNavigator = StackNavigator(
+    {
+        PhotosListScreen: {
+            screen: PhotosListScreen
+        },
+        PhotoSingleScreen: {
+            screen: PhotoSingleScreen
+        }
+    }
+);
