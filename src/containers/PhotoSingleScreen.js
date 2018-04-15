6e33ec70ecb7cf03c91b0131d6a97971caa2e9c3
Zarko Dencic <denciczarko@gmail.com>
2018-04-15 18:50:51 +0200

    prettier

--- src/containers/PhotoSingleScreen.js
+++ src/containers/PhotoSingleScreen.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { View, Text } from 'react-native';
+// import PropTypes from 'prop-types';
+import { Text } from 'react-native';
 
 class PhotoSingle extends Component {
   render() {
