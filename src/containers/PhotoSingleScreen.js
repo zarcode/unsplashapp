55649140b9b4fddde8fbe6cbce1013c3c391ed39
Zarko Dencic <denciczarko@gmail.com>
2018-04-15 18:49:47 +0200

    prettier + linter

--- src/containers/PhotoSingleScreen.js
+++ src/containers/PhotoSingleScreen.js
@@ -1,13 +1,11 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import {View, Text} from "react-native";
+import { View, Text } from 'react-native';
 
 class PhotoSingle extends Component {
-    render() {
-        return (
-            <Text>Single</Text>
-        );
-    }
+  render() {
+    return <Text>Single</Text>;
+  }
 }
 
 PhotoSingle.propTypes = {};
--- src/reducers/nav.js
+++ src/reducers/nav.js
@@ -1,24 +1,19 @@
 // @flow
 
-import {NavigationActions} from "react-navigation";
-import {AppNavigator} from "../navigation/AppNavigator";
+import { NavigationActions } from 'react-navigation';
+import { AppNavigator } from '../navigation/AppNavigator';
 
-const initialState = AppNavigator.router.getStateForAction(
-    NavigationActions.init()
-);
+const initialState = AppNavigator.router.getStateForAction(NavigationActions.init());
 
-export const nav = (
-    state: * = initialState,
-    action: *
-) => {
-    switch (action.type) {
-        case "PhotoSingleScreen":
-            return AppNavigator.router.getStateForAction(
-                NavigationActions.navigate({routeName: "PhotoSingleScreen"}),
-                state
-            );
-        default:
-            const newState = AppNavigator.router.getStateForAction(action, state);
-            return newState || state;
-    }
+export const nav = (state: * = initialState, action: *) => {
+  const newState = AppNavigator.router.getStateForAction(action, state);
+  switch (action.type) {
+    case 'PhotoSingleScreen':
+      return AppNavigator.router.getStateForAction(
+        NavigationActions.navigate({ routeName: 'PhotoSingleScreen' }),
+        state,
+      );
+    default:
+      return newState || state;
+  }
 };
