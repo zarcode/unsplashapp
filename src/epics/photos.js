5a9ee3b8bc8c25a50c1189ac112746a645a01e30
Zarko Dencic <denciczarko@gmail.com>
2018-04-23 16:07:58 +0200

    list filters

--- src/epics/photos.js
+++ src/epics/photos.js
@@ -13,14 +13,36 @@ import { Action } from '../action/actionTypes';
 import * as photosActions from '../action/photos';
 import { asObservable } from './rxUtils';
 import api from '../api';
+import type { PhotosFilter } from '../api/types';
 
-export const loadPhotosToList = (action: Observable<Action>): Observable<Action> =>
-  action.ofType(ACTION.FETCH_PHOTOS_REQUESTED).mergeMap((a) => {
-    const loadingAction = Observable.of(photosActions.photosLoading());
-    const requestAction = asObservable(api.fetchPhotos({}))
-      .map(data => photosActions.photosSuccess(data, a.filter))
-      .catch(e => Observable.of(photosActions.photosFail(e.message)));
-    return Observable.concat(loadingAction, requestAction)
-      .takeUntil(action
-        .filter(futureAction => futureAction.type === ACTION.FETCH_PHOTOS_REQUESTED));
-  });
+const perPage = 30;
+
+export const loadPhotosToList = (action: Observable<Action>, store: Object): Observable<Action> => {
+  const state = (filter: PhotosFilter) => store.getState().photos[filter];
+  return action
+    // .ofType(ACTION.FETCH_PHOTOS_REQUESTED)
+    .filter((a: Action) =>
+      a.type === ACTION.FETCH_PHOTOS_REQUESTED &&
+      ((state(a.filter).loadingState === 'idle' && !state(a.filter).isLastPage) || a.refresh))
+    .mergeMap((a) => {
+      const nextPage = !a.refresh ? state(a.filter).lastLoadedPage + 1 : 1;
+      const loadingAction = Observable.of(photosActions.photosLoading(a.filter, a.refresh));
+      const requestAction = asObservable(api.fetchPhotos({
+        page: nextPage,
+        per_page: perPage,
+        order_by: a.filter,
+      }))
+        .map(data =>
+          photosActions.photosSuccess(
+            data,
+            a.filter,
+            nextPage,
+            data.length < perPage,
+            a.refresh,
+          ))
+        .catch(e => Observable.of(photosActions.photosFail(e.message)));
+      return Observable.concat(loadingAction, requestAction)
+        .takeUntil(action
+          .filter(futureAction => futureAction.type === ACTION.FETCH_PHOTOS_REQUESTED));
+    });
+}
