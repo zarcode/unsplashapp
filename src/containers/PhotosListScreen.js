264203d8da9d4c2bdd88a7609b76e9a39caf9a56
Zarko Dencic <denciczarko@gmail.com>
2018-04-24 13:58:16 +0200

    move filters to header

--- src/containers/PhotosListScreen.js
+++ src/containers/PhotosListScreen.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
+import { Header } from 'react-navigation';
 import PhotosFilters from './PhotosFilters';
 import PhotosList from './PhotosList';
 
@@ -9,15 +10,31 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  header: {
+    height: Header.HEIGHT,
+    justifyContent: 'flex-end',
+    backgroundColor: 'white',
+  },
 });
 
 const PhotosListScreen = () => (
   <View
     style={styles.container}
   >
-    <PhotosFilters />
     <PhotosList />
   </View>
 );
+
+PhotosListScreen.navigationOptions = () => {
+  return {
+    header: (
+      <View
+        style={styles.header}
+      >
+        <PhotosFilters />
+      </View>
+    ),
+  };
+};
 
 export default PhotosListScreen;
