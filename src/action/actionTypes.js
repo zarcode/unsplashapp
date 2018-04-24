9c101849c6979b66ea697b4df897094c506893f7
Zarko Dencic <denciczarko@gmail.com>
2018-04-24 13:43:31 +0200

    fix list loading

--- src/action/actionTypes.js
+++ src/action/actionTypes.js
@@ -1,11 +1,33 @@
 import { ACTION } from '../constants';
 import type { PhotosFilter } from '../api/types';
 
-export type PhotosAction =
-	{
-		type: ACTION.FETCH_PHOTOS_REQUESTED,
-		filter: PhotosFilter,
-	} |
-  { type: string }
+export type PhotosAction = {
+    type: ACTION.FETCH_PHOTOS_REQUESTED,
+    filter: PhotosFilter,
+  } |
+  {
+    type: ACTION.FETCH_PHOTOS_REQUESTED,
+    filter: PhotosFilter,
+    refresh: boolean
+  } |
+  {
+    type: ACTION.FETCH_PHOTOS_LOADING,
+    filter: PhotosFilter,
+    refresh: boolean
+  } |
+  {
+    type: ACTION.FETCH_PHOTOS_SUCCESS,
+    response: {
+      entities: {
+        users: any,
+        photos: any,
+      },
+      result: Array<string>
+    },
+    filter: PhotosFilter,
+    page: number,
+    isLastPage: boolean,
+    refresh: boolean,
+  }
 
 export type Action = PhotosAction;
