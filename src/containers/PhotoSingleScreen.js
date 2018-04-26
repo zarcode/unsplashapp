956b2b8b9c82ae5b77dbfe7b597770f67c2098c1
Zarko Dencic <denciczarko@gmail.com>
2018-04-26 13:35:49 +0200

    preview photo

--- src/containers/PhotoSingleScreen.js
+++ src/containers/PhotoSingleScreen.js
@@ -4,14 +4,57 @@
 
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
-import { Text } from 'react-native';
+import PhotoView from 'react-native-photo-view';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Header } from 'react-navigation';
 
-class PhotoSingle extends Component {
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  imagePreview: {
+    flex: 1,
+    width: '100%',
+  },
+  topLine: {
+    position: 'absolute',
+    height: Header.HEIGHT,
+    justifyContent: 'flex-end',
+  },
+  backButton: {
+    backgroundColor: 'grey',
+  },
+});
+
+class PhotoSingleScreen extends Component {
   render() {
-    return <Text>Single</Text>;
+    return (
+      <View style={styles.container}>
+        <PhotoView
+          source={{
+            uri: 'https://images.unsplash.com/photo-1524562596000-38f0dbe5b07a?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjI0NzMwfQ&s=6642c2d7e15fc4a1d2b0d8bd65835e65',
+          }}
+          minimumZoomScale={0.9}
+          maximumZoomScale={3}
+          androidScaleType="center"
+          onLoad={() => console.log('Image loaded!')}
+          style={styles.imagePreview}
+        />
+        <View style={styles.topLine}>
+          <TouchableOpacity
+            style={styles.backButton}
+          >
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
   }
 }
 
-PhotoSingle.propTypes = {};
+PhotoSingleScreen.navigationOptions = () => {
+  return {
+    header: null,
+  };
+};
 
-export default PhotoSingle;
+export default PhotoSingleScreen;
