fba0a58d19935df5d74bd3c0fb436de485be41a0
Zarko Dencic <denciczarko@gmail.com>
2018-06-06 09:00:01 +0200

    test more cover on filters

--- src/containers/PhotosFilters.test.js
+++ src/containers/PhotosFilters.test.js
@@ -3,18 +3,23 @@ import { filters } from '../reducers/photos';
 
 describe('<PhotosFilters />', () => {
   it('trigger filter actions correctly', () => {
-    const props = {
-      currentFilter: filters[0].id,
-      lastLoadedPage: () => 0,
+    const props = (lastLoadedPage, currentFilter) => ({
+      currentFilter,
+      lastLoadedPage: () => lastLoadedPage,
       actions: {
         photosRequested: jest.fn(),
         changeFilter: jest.fn(),
       },
-    };
+    });
+    const propsFirstPage = props(0, filters[0].id);
+    handleChangeFilter(propsFirstPage)(filters[0].id)();
+    expect(propsFirstPage.actions.photosRequested.mock.calls.length).toBe(0);
+    expect(propsFirstPage.actions.changeFilter.mock.calls.length).toBe(0);
+    handleChangeFilter(propsFirstPage)(filters[1].id)();
+    expect(propsFirstPage.actions.photosRequested.mock.calls.length).toBe(1);
 
-    handleChangeFilter(props)(filters[1].id)();
-
-    expect(props.actions.changeFilter.mock.calls.length).toBe(0);
-    expect(props.actions.photosRequested.mock.calls.length).toBe(1);
+    const propsNotFirstPage = props(1, filters[0].id);
+    handleChangeFilter(propsNotFirstPage)(filters[1].id)();
+    expect(propsNotFirstPage.actions.changeFilter.mock.calls.length).toBe(1);
   });
 });
