72f9a7e09e4b5f296b13973285be0123e352d91d
Zarko Dencic <denciczarko@gmail.com>
2018-06-06 08:46:37 +0200

    test fileters

--- __tests__/Root.test.js
+++ __tests__/Root.test.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import toJson from 'enzyme-to-json';
 import { shallow } from 'enzyme';
 // Note: test renderer must be required after react-native.
 // import renderer from 'react-test-renderer';
-import Root, { AppWithNavigationState } from '../src/Root';
+import { AppNavigator } from '../src/navigation/AppNavigator';
+import Root, { App, AppWithNavigationState } from '../src/Root';
 
 it('<Root /> renders correctly', () => {
   const wrapper = shallow(<Root />);
   expect(wrapper.find(AppWithNavigationState).length).toBe(1);
+});
+it('<App /> renders correctly', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find(AppNavigator).length).toBe(1);
 });
