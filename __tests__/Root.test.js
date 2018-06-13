be1463a0accd37fa04dbcfb7a96cb702de1a25e8
Zarko Dencic <denciczarko@gmail.com>
2018-06-13 12:11:06 +0200

    fix imports

--- __tests__/Root.test.js
+++ __tests__/Root.test.js
@@ -3,8 +3,8 @@ import React from 'react';
 import { shallow } from 'enzyme';
 // Note: test renderer must be required after react-native.
 // import renderer from 'react-test-renderer';
-// import AppNavigator from '../src/navigation/AppNavigator';
-import Root, { AppWithNavigationState } from '../src/Root';
+import AppNavigator from '../src/navigation/AppNavigator';
+import Root, { App, AppWithNavigationState } from '../src/Root';
 // App,
 
 it('<Root /> renders correctly', () => {
@@ -12,6 +12,6 @@ it('<Root /> renders correctly', () => {
   expect(wrapper.find(AppWithNavigationState).length).toBe(1);
 });
 it('<App /> renders correctly', () => {
-  // const wrapper = shallow(<App />);
-  // expect(wrapper.find(AppNavigator).length).toBe(1);
+  const wrapper = shallow(<App />);
+  expect(wrapper.find(AppNavigator).length).toBe(1);
 });
--- src/containers/PhotosList.test.js
+++ src/containers/PhotosList.test.js
@@ -4,21 +4,23 @@ import { shallow } from 'enzyme';
 import { PhotosListComponent as PhotosList } from './PhotosList';
 
 describe('<PhotosFilters />', () => {
+  const props = {
+    filter: 'latest',
+    isLastPage: false,
+    loadingState: 'idle',
+    lastLoadedPage: 1,
+    actions: {
+      photosRequested: jest.fn(),
+    },
+  };
+  const wrapper = shallow(<PhotosList {...props} />);
   it('setProps calls componentWillReceiveProps', () => {
-    const props = {
-      loadingState: 'idle',
-      lastLoadedPage: 0,
-      actions: {
-        photosRequested: jest.fn(),
-      },
-    };
     jest.spyOn(PhotosList.prototype, 'componentWillReceiveProps');
-    const wrapper = shallow(<PhotosList {...props} />);
     wrapper.setProps({ getErrorMessage: 'Error' });
     expect(PhotosList.prototype.componentWillReceiveProps.mock.calls.length).toBe(1);
   });
   it('componentDidMount calls photosRequested', () => {
-    const props = {
+    const props1 = {
       loadingState: 'idle',
       lastLoadedPage: 0,
       filter: 'latest',
@@ -27,21 +29,17 @@ describe('<PhotosFilters />', () => {
       },
     };
     jest.spyOn(PhotosList.prototype, 'componentDidMount');
-    shallow(<PhotosList {...props} />);
+    shallow(<PhotosList {...props1} />);
     // wrapper.instance().onStart();
     expect(PhotosList.prototype.componentDidMount).toBeCalled();
-    expect(props.actions.photosRequested).toBeCalledWith('latest', true);
+    expect(props1.actions.photosRequested).toBeCalledWith('latest', true);
   });
   it('refresh calls photosRequested', () => {
-    const props = {
-      filter: 'latest',
-      actions: {
-        photosRequested: jest.fn(),
-      },
-    };
-    const wrapper = shallow(<PhotosList {...props} />);
     wrapper.instance().refresh();
-    expect(props.actions.photosRequested.mock.calls.length).toBe(1);
-    expect(props.actions.photosRequested.mock.calls[0][0]).toBe('latest');
+    expect(props.actions.photosRequested).toBeCalledWith('latest', true);
+  });
+  it('loadMore calls photosRequested', () => {
+    wrapper.instance().loadMore();
+    expect(props.actions.photosRequested).toBeCalledWith('latest', false);
   });
 });
