5031822c3bcf2c2f4129c40d7578dd63e1da43d7
Zarko Dencic <denciczarko@gmail.com>
2018-06-20 20:12:56 +0200

    prettify

---
+++ src/components/PhotoSingleScreen/PhotoSingleScreen.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import toJson from 'enzyme-to-json';
+import { shallow } from 'enzyme';
+import PhotoSingleScreen from './PhotoSingleScreen';
+
+describe('<PhotoSingleScreen>', () => {
+  it('renders correctly', () => {
+    const props = {
+      photo: {
+        url: 'url',
+      },
+    };
+    const wrapper = shallow(<PhotoSingleScreen {...props} />);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+});
