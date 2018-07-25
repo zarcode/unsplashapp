5aa978306a99f3541888f6979e0cbbefcb98e43c
Zarko Dencic <denciczarko@gmail.com>
2018-07-25 21:11:40 +0200

    new component for photos

--- src/components/PhotosListScreen/PhotosListScreen.test.js
+++ src/components/PhotosListScreen/PhotosListScreen.test.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { shallow } from 'enzyme';
 // Note: test renderer must be required after react-native.
 import PhotosListScreen from './index';
-import PhotosList from './PhotosList';
+import AllPhotosList from './AllPhotosList';
 
 it('<PhotosListScreen/> renders correctly', () => {
   const wrapper = shallow(<PhotosListScreen />);
-  expect(wrapper.find(PhotosList).length).toBe(1);
+  expect(wrapper.find(AllPhotosList).length).toBe(1);
 });
