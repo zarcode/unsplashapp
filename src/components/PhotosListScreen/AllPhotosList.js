fe6e6a41aa1a9bea7bbe3e0cc78d02db34ac2a75
Zarko Dencic <denciczarko@gmail.com>
2018-07-25 21:11:40 +0200

    new component for photos

---
+++ src/components/PhotosListScreen/AllPhotosList.js
@@ -0,0 +1,40 @@
+// @flow
+import { connect } from 'react-redux';
+import { mapProps } from 'recompose';
+import { bindActionCreators } from 'redux';
+
+import { photosRequested, toSinglePhoto } from '../../action/photos';
+import {
+  getPhotos,
+  getFilter,
+  getErrorMessage,
+  getIsLastPage,
+  getLastLoadedPage,
+  getLoadingState,
+} from '../../reducers/photos';
+import PhotosList from '../shared/PhotosList';
+
+const mapPropsHoc = mapProps(x => x);
+
+export const AllPhotosList = mapPropsHoc(PhotosList);
+
+const mapStateToProps = (state) => {
+  const filter = getFilter(state);
+  return {
+    photos: getPhotos(state),
+    filter,
+    isLastPage: getIsLastPage(state),
+    lastLoadedPage: getLastLoadedPage(state)(filter),
+    loadingState: getLoadingState(state),
+    getErrorMessage: getErrorMessage(state),
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators({ photosRequested, toSinglePhoto }, dispatch),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(AllPhotosList);
